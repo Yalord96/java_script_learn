cce8b58070044be1bd5974aff080d293b4a8333b
Yalord96 <sergeynosenko96@gmail.com>
2020-06-18 23:43:47 +0300

    first hw

---
+++ main.js
@@ -0,0 +1,27 @@
+var message;
+var n = 123;
+var str = 'Дима намазал волосы гелем на прошлом занятии';
+var logic = true;
+var nothing = null;
+var x;
+
+var years = prompt('Сколько тебе лет?', '');
+var year = 2020;
+
+
+alert('Вы родились в  ' +(year-years)+ ' году!');
+
+var dollar = prompt('Курс доллара: ', 26.86);
+var uah = prompt('Сколько у тебя грн? ', '');
+
+    alert('У тебя получается: '+(dollar*uah)+'долларов!');
+
+var gel = confirm("Дима мазал гелем волосы на прошлом занятии?");
+
+alert(gel);
+
+console.log(typeof 42);
+console.log(typeof 'qwe');
+console.log(typeof true);
+console.log(typeof dasdasdas);
+console.log(typeof {a: 1});
