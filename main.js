bb2c905eff6c45bfc7e6b11202f98d7a5f69959d
Yalord96 <sergeynosenko96@gmail.com>
2020-06-29 22:28:53 +0300

    jquery

--- main.js
+++ main.js
@@ -1,77 +1,86 @@
-var js = prompt('Каково оффициальное название "Javascript"?','');
-
-
-if (js == 'ECMAScript') {
-    alert('Верно!');
-}
-else {
-    alert('не знаете? "ECMAScript"!');
-}
-
-var value = prompt('Укажите значение','');
-if (value > 0){
-    alert('1');
-}
-else if (value < 0){
-    alert('-1');
-}
- else if (value == 0){
-     alert('0');
+function ecmaScript() {
+    var js = prompt('Каково оффициальное название "Javascript"?', '');
+    if (js == 'ECMAScript') {
+        alert('Верно!');
+    } else {
+        alert('не знаете? "ECMAScript"!');
+    }
 }
 
-var admin = prompt('Введите логин:','');
-
- if (admin == 'admin') {
-     var password = prompt('Введите пароль', '');
-     if (password == 'Чёрный властелин'){
-         alert('Добро пожаловать!');}
-     else {
-         alert('пароль неверен!');
-     }
- }
- else {
-     alert('Я Вас не знаю');
- }
-
-
-
-
-var i = 0;
-while (i < 3){
-    alert(i);
-    i++;
+function putValue() {
+    var value = prompt('Укажите значение', '');
+    if (value > 0) {
+        alert('1');
+    } else if (value < 0) {
+        alert('-1');
+    } else if (value == 0) {
+        alert('0');
+    }
 }
 
-var i = 0;
+function admin() {
+    var admin = prompt('Введите логин:', '');
 
-do {
-    i = prompt('введи число','');
+    if (admin === 'admin') {
+        var password = prompt('Введите пароль', '');
+        if (password == 'Чёрный властелин') {
+            alert('Добро пожаловать!');
+        } else {
+            alert('пароль неверен!');
+        }
+    } else {
+        alert('Я Вас не знаю');
+    }
 }
-while (i != 100);
+
+function whileValue() {
+    var i = 0;
+    while (i < 3) {
+        alert(i);
+        i++;
+    }
+}
+
+function value100() {
+    var i = 0;
+
+    do {
+        i = prompt('введи число', '');
+    }
+    while (i != 100);
+    alert('Вы ввели число : 100');
+}
 
 
-alert('Дима намазал голову гелем');
+function year() {
+    var years = prompt('Сколько тебе лет?', '');
+    var year = 2020;
 
 
+    alert('Вы родились в  ' + (year - years) + ' году!');
+}
 
 
-// var years = prompt('Сколько тебе лет?', '');
-// var year = 2020;
-//
-//
-// alert('Вы родились в  ' +(year-years)+ ' году!');
-//
-// var dollar = prompt('Курс доллара: ', 26.86);
-// var uah = prompt('Сколько у тебя грн? ', '');
-//
-//     alert('У тебя получается: '+(dollar*uah)+'долларов!');
-//
-// var gel = confirm("Дима мазал гелем волосы на прошлом занятии?");
-//
-// alert(gel);
-//
-// console.log(typeof 42);
-// console.log(typeof 'qwe');
-// console.log(typeof true);
-// console.log(typeof dasdasdas);
-// console.log(typeof {a: 1});
+function dollar() {
+    var dollar = prompt('Курс доллара: ', 26.6);
+    var uah = prompt('Сколько у тебя грн? ', '');
+
+    alert('У тебя получается: ' + (dollar * uah) + 'долларов!');
+}
+
+
+$(function (){
+    var current = $("div#mass p").first();
+    $('#next').click(function (){
+        $(current).not('.last').hide("fast", function () {
+            current = $(current).next('p');
+            $(current).show("fast");
+        });
+    });
+    $('#prev').click(function (){
+        $(current).not('.first').hide("fast", function () {
+            current = $(current).prev('p');
+            $(current).show("fast");
+        });
+    });
+});
